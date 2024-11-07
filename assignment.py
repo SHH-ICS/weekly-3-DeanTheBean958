49b34d93eb43f6b55ab38928bb22a372cd12c624
DeanTheBean958 <dean.pauls@stu.ocsb.ca>
2024-11-07 14:53:35 +0000

    pie

--- assignment.py
+++ assignment.py
@@ -1,1 +1,19 @@
-
+pi = input()
+chars = set('0123456789')
+if Pi == "LG" or Pi == "XL":
+  Toppings = input()
+  if all((c in chars) for c in Toppings):
+    if Toppings > 0:
+      Price = (Toppings-1)*0.75 + 1
+    else:
+      Price = 0
+    if Pi == "LG":
+      Price = Price + 6
+  elif Pi == "XL":
+    Price = Price + 10
+  Price = Price*1.13
+  print("The total is: $" + str(Price))
+  else:
+    print("Invalid Toppings")
+else:
+  print("Invalid Size")
